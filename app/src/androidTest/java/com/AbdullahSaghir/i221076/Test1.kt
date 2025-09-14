2a194768cb88d5aa2959afe9fced4dd10f6546c2
Abdullahsaghir333 <i221076@nu.edu.pk>
2025-09-14 21:50:53 +0500

    third commit

--- app/src/androidTest/java/com/AbdullahSaghir/i221076/Test1.kt
+++ app/src/androidTest/java/com/AbdullahSaghir/i221076/Test1.kt
@@ -1,23 +1,18 @@
 package com.AbdullahSaghir.i221076
 
-
 import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.AllOf.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,60 +23,25 @@ class Test1 {
 
     @Rule
     @JvmField
-    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    var mActivityScenarioRule = ActivityScenarioRule(login::class.java)
 
     @Test
     fun test1() {
-        val materialTextView = onView(
+        // Find the button by ID and click it
+        val loginBtn = onView(
             allOf(
-                withId(R.id.logintbn1), withText("Log in"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("android.widget.LinearLayout")),
-                        3
-                    ),
-                    0
-                ),
+                withId(R.id.logintbn1),
                 isDisplayed()
             )
         )
-        materialTextView.perform(click())
-
-        val materialButton = onView(
-            allOf(
-                withId(R.id.loginButton), withText("Log in"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    6
-                ),
-                isDisplayed()
-            )
-        )
-        materialButton.perform(click())
-
-        val appCompatImageView = onView(
-            allOf(
-                withId(R.id.mainsearch), withContentDescription("Search"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("android.widget.LinearLayout")),
-                        1
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageView.perform(click())
+        loginBtn.perform(click())
     }
 
+    // Helper method for finding child views at specific positions
     private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
+        parentMatcher: Matcher<View>,
+        position: Int
     ): Matcher<View> {
-
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
                 description.appendText("Child at position $position in parent ")
@@ -90,8 +50,8 @@ class Test1 {
 
             public override fun matchesSafely(view: View): Boolean {
                 val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
+                return parent is ViewGroup && parentMatcher.matches(parent) &&
+                        view == parent.getChildAt(position)
             }
         }
     }
